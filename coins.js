b99603c885e5fa97462e939f2f8dfb354bf91fa5
Sarah Morris <sarah.morris9@gmail.com>
2017-12-05 16:06:37 -0600

    Finished challenge

--- coins.js
+++ coins.js
@@ -10,15 +10,35 @@
 
 */
 
-function coinCounter () {
-  // Initialize a JavaScript object to hold the coins
-  var coinPurse = {};
-  
-  coinPurse.quarters = 0;
+let coinPurse = {
+  quarters: 0,
+  dimes: 0,
+  nickels: 0, 
+  pennies: 0
+};
+
+function coinCounter(dollar) {
+  dollar = dollar * 100;
+
+  coinPurse.quarters = Math.floor(dollar / 25);
+    dollar = dollar % 25;
+
+  coinPurse.dimes = Math.floor(dollar / 10);
+    dollar = dollar % 10;
+
+  coinPurse.nickels = Math.floor(dollar / 5);
+    dollar = dollar % 5;
+
+  coinPurse.pennies = Math.floor(dollar / 1);
+    dollar = dollar % 1;
 
   return coinPurse;
 }
 
-var coins = coinCounter()
-console.log();
+console.log(coinCounter(2.04)); 
+console.log(coinCounter(.78));
+console.log(coinCounter(2.27));
+
+
+
 
